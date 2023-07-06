d06a531654052e0085f75b759d6b5b3a28409c02
Myla Voronevska <code.crystal.008@gmail.com>
2023-07-06 09:02:46 +0300

    Updates

--- index.68a15e1e.js
+++ index.68a15e1e.js
@@ -1,2 +1,16 @@
+function changeBrand() {
+  const { classList: googleClassList } = document.querySelector("#logo__image--google");
+  const { classList: oddityClassList } = document.querySelector("#logo__image--oddity");
+  const brands = document.querySelectorAll(".brand");
+  googleClassList.toggle("logo__image--hidden");
+  oddityClassList.toggle("logo__image--hidden");
+  brands.forEach((brand)=>{
+      brand.classList.toggle("brand--hidden");
+  });
+  const body = document.querySelector("body");
+  const currentTheme = body.getAttribute("data-theme");
+  const newTheme = currentTheme === "oddity" ? "" : "oddity";
+  body.setAttribute("data-theme", newTheme);
+}
 
 //# sourceMappingURL=index.68a15e1e.js.map
